abc75214dabb0c1a4fdd1bd3ca8e7f353e529c4b
Shin Yamamoto <shin@scenee.com>
2025-03-01 19:16:25 +0900

    Modify test case labels

--- test/tlv_test.dart
+++ test/tlv_test.dart
@@ -13,27 +13,27 @@ void main() {
       expect(tlv.data, [0x5F, 0x24, 0x03, 0x97, 0x03, 0x31]);
     });
 
-    test("lengthBytes single byte", () {
+    test("length single byte", () {
       final tlv = TLV(tag: [0x5F, 0x24], value: List.filled(127, 0));
       expect(tlv.length, [0x7F]);
     });
 
-    test("lengthBytes two bytes", () {
+    test("length two bytes", () {
       final tlv = TLV(tag: [0x5F, 0x24], value: List.filled(128, 0));
       expect(tlv.length, [0x81, 0x80]);
     });
 
-    test("lengthBytes three bytes", () {
+    test("length three bytes", () {
       final tlv = TLV(tag: [0x5F, 0x24], value: List.filled(256, 0));
       expect(tlv.length, [0x82, 0x01, 0x00]);
     });
 
-    test("lengthBytes four bytes", () {
+    test("length four bytes", () {
       final tlv = TLV(tag: [0x5F, 0x24], value: List.filled(65536, 0));
       expect(tlv.length, [0x83, 0x01, 0x00, 0x00]);
     });
 
-    test("lengthBytes five bytes", () {
+    test("length five bytes", () {
       final tlv = TLV(tag: [0x5F, 0x24], value: List.filled(16777216, 0));
       expect(tlv.length, [0x84, 0x01, 0x00, 0x00, 0x00]);
     });
