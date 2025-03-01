66ba0b609320829a72ade35a1b737509c93227db
Shin Yamamoto <shin@scenee.com>
2025-03-01 20:24:43 +0900

    Improve TLVParser implementation for the performance

--- lib/src/tlv/tlv_parser.dart
+++ lib/src/tlv/tlv_parser.dart
@@ -18,51 +18,51 @@ class TLVParser {
 
   /// Parses a TLV object from a list of bytes starting at the given index.
   static (TLV, int) _parseTLV(List<int> data, int i) {
-    // Tag
-    List<int> tag = [];
+    final emptyTLV = TLV(tag: [], value: []);
     int offset = 0;
+
+    if (i + offset >= data.length) return (emptyTLV, 0);
+
+    // Parsing the tag
     final first = data[i];
     if ((first & 0x1f) == 0x1f) {
       // long tag field
-      tag.add(first);
       offset += 1;
       while (data[i + offset] & 0x80 != 0) {
-        tag.add(data[i + offset]);
         offset += 1;
       }
-      tag.add(data[i + offset]);
       offset += 1;
     } else {
       // short tag field
-      tag = [first];
       offset += 1;
     }
 
-    if (i + offset >= data.length) {
-      return (TLV(tag: [], value: []), 0);
-    }
+    if (i + offset >= data.length) return (emptyTLV, 0);
 
-    // Length
+    // Tag
+    final tag = data.sublist(i, i + offset);
+
+    // Paring the value length
     int length = 0;
-    final len1 = data[i + offset];
-    if (len1 < 0x80) {
-      length = len1;
+    final lenFirst = data[i + offset];
+    if (lenFirst < 0x80) {
+      length = lenFirst;
       offset += 1;
-    } else if (len1 == 0x81) {
+    } else if (lenFirst == 0x81) {
       if (i + offset + 1 >= data.length) return (TLV(tag: [], value: []), 0);
       length = data[i + offset + 1];
       offset += 2;
-    } else if (len1 == 0x82) {
+    } else if (lenFirst == 0x82) {
       if (i + offset + 2 >= data.length) return (TLV(tag: [], value: []), 0);
       length = (data[i + offset + 1] << 8) + data[i + offset + 2];
       offset += 3;
-    } else if (len1 == 0x83) {
+    } else if (lenFirst == 0x83) {
       if (i + offset + 3 >= data.length) return (TLV(tag: [], value: []), 0);
       length = (data[i + offset + 1] << 16) +
           (data[i + offset + 2] << 8) +
           data[i + offset + 3];
       offset += 4;
-    } else if (len1 == 0x84) {
+    } else if (lenFirst == 0x84) {
       if (i + offset + 4 >= data.length) return (TLV(tag: [], value: []), 0);
       length = (data[i + offset + 1] << 24) +
           (data[i + offset + 2] << 16) +
@@ -70,12 +70,10 @@ class TLVParser {
           data[i + offset + 4];
       offset += 5;
     } else {
-      return (TLV(tag: [], value: []), 0);
+      return (emptyTLV, 0);
     }
 
-    if (i + offset + length > data.length) {
-      return (TLV(tag: [], value: []), 0);
-    }
+    if (i + offset + length > data.length) return (emptyTLV, 0);
 
     // Value
     final value = data.sublist(i + offset, i + offset + length);
--- test/tlv_parser_test.dart
+++ test/tlv_parser_test.dart
@@ -7,27 +7,27 @@ void main() {
       final tlvs = TLVParser.parse([0x59, 0x02, 0x95, 0x02]);
       expect(tlvs.length, equals(1));
 
-      final do1 = tlvs[0];
-      expect(do1.tag, equals([0x59]));
-      expect(do1.value.length, equals(2));
-      expect(do1.value, equals([0x95, 0x02]));
-      expect(do1.isConstructed(), false);
+      final tlv = tlvs[0];
+      expect(tlv.tag, equals([0x59]));
+      expect(tlv.value.length, equals(2));
+      expect(tlv.value, equals([0x95, 0x02]));
+      expect(tlv.isConstructed(), false);
     });
 
     test("Parse long tag and short length", () {
-      final tlvs = TLVParser.parse([0x5F, 0x24, 0x03, 0x97, 0x03, 0x31]);
+      final tlvs = TLVParser.parse([0x5F, 0x24, 0x03, 0x42, 0x42, 0x42]);
       expect(tlvs.length, equals(1));
 
-      final do2 = tlvs[0];
-      expect(do2.tag, equals([0x5F, 0x24]));
-      expect(do2.value.length, equals(3));
-      expect(do2.value, equals([0x97, 0x03, 0x31]));
-      expect(do2.isConstructed(), false);
+      final tlv = tlvs[0];
+      expect(tlv.tag, equals([0x5F, 0x24]));
+      expect(tlv.value.length, equals(3));
+      expect(tlv.value, equals([0x42, 0x42, 0x42]));
+      expect(tlv.isConstructed(), false);
     });
 
     test("Parse multiple TLVs", () {
       final tlvs = TLVParser.parse(
-          [0x59, 0x02, 0x95, 0x02, 0x5F, 0x24, 0x03, 0x97, 0x03, 0x31]);
+          [0x59, 0x02, 0x95, 0x02, 0x5F, 0x24, 0x03, 0x42, 0x42, 0x42]);
       expect(tlvs.length, equals(2));
 
       final do1 = tlvs[0];
@@ -38,19 +38,60 @@ void main() {
       final do2 = tlvs[1];
       expect(do2.tag, equals([0x5F, 0x24]));
       expect(do2.value.length, equals(3));
-      expect(do2.value, equals([0x97, 0x03, 0x31]));
+      expect(do2.value, equals([0x42, 0x42, 0x42]));
     });
 
-    test("Parse long tag and long length", () {
-      final tlvs = TLVParser.parse(
-          [0x5F, 0x81, 0x24, 0x82, 0x01, 0x00] + List.filled(256, 0x97));
+    test("Parse long tag and long length (0x81)", () {
+      final tag = [0x5F, 0x81, 0x7F]; // 0x81 is dummy
+      final value = List.filled(255, 0x42);
+      final tlvs = TLVParser.parse(tag + [0x81, 0xFF] + value);
       expect(tlvs.length, equals(1));
 
-      final do3 = tlvs[0];
-      expect(do3.tag, equals([0x5F, 0x81, 0x24]));
-      expect(do3.value.length, equals(256));
-      expect(do3.value, equals(List.filled(256, 0x97)));
-      expect(do3.isConstructed(), false);
+      final tlv = tlvs[0];
+      expect(tlv.tag, tag);
+      expect(tlv.value.length, equals(255));
+      expect(tlv.value, equals(value));
+      expect(tlv.isConstructed(), false);
+    });
+
+    test("Parse long tag and long length (0x82)", () {
+      final tag = [0x5F, 0x82, 0x7F]; // 0x82 is dummy
+      final value = List.filled(256, 0x42);
+      final tlvs = TLVParser.parse(tag + [0x82, 0x01, 0x00] + value);
+      expect(tlvs.length, equals(1));
+
+      final tlv = tlvs[0];
+      expect(tlv.tag, tag);
+      expect(tlv.value.length, 256);
+      expect(tlv.value, value);
+      expect(tlv.isConstructed(), false);
+    });
+
+    test("Parse long tag and long length (0x83)", () {
+      final tag = [0x5F, 0x83, 0x7F]; // 0x83 is dummy
+      final value = List.filled(0x10000, 0x42);
+      final tlvs = TLVParser.parse(tag + [0x83, 0x01, 0x00, 0x00] + value);
+      expect(tlvs.length, equals(1));
+
+      final tlv = tlvs[0];
+      expect(tlv.tag, tag);
+      expect(tlv.value.length, 0x10000);
+      expect(tlv.value, value);
+      expect(tlv.isConstructed(), false);
+    });
+
+    test("Parse long tag and long length (0x84)", () {
+      final tag = [0x5F, 0x84, 0x7F]; // 0x84 is dummy
+      final value = List.filled(0x1000000, 0x42);
+      final tlvs =
+          TLVParser.parse(tag + [0x84, 0x01, 0x00, 0x00, 0x00] + value);
+      expect(tlvs.length, equals(1));
+
+      final tlv = tlvs[0];
+      expect(tlv.tag, tag);
+      expect(tlv.value.length, 0x1000000);
+      expect(tlv.value, value);
+      expect(tlv.isConstructed(), false);
     });
 
     test("Parse invalid TLV", () {
